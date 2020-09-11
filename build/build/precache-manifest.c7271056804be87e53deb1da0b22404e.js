9b18ac752331a3635300298bacc6fa960948e403
Agbanusi <agbanusijohn@gmail.com>
2020-09-11 11:24:27 +0100

    finally done!

---
+++ build/build/precache-manifest.c7271056804be87e53deb1da0b22404e.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb3a9cdaba7150a11041ed777c1e615d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "345ce80058f9689eac4c",
+    "url": "/static/css/main.78438787.chunk.css"
+  },
+  {
+    "revision": "db98a9146c126fc1169c",
+    "url": "/static/js/2.20f8078b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.20f8078b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "345ce80058f9689eac4c",
+    "url": "/static/js/main.78203285.chunk.js"
+  },
+  {
+    "revision": "48f6bdb2d8f118f185b8",
+    "url": "/static/js/runtime-main.086302fa.js"
+  },
+  {
+    "revision": "67c89b87ca11669737d733b329b1ef6f",
+    "url": "/static/media/OAYTMM0.67c89b87.jpg"
+  },
+  {
+    "revision": "1ee1a6d365150dd3ce46a78a77213dc5",
+    "url": "/static/media/OSO1XY0.1ee1a6d3.jpg"
+  },
+  {
+    "revision": "00f6cfd2a04102c132bcf2bdf821a428",
+    "url": "/static/media/automobile-958185_640.00f6cfd2.jpg"
+  },
+  {
+    "revision": "1e5257595885acfbc1912f10ce4bebae",
+    "url": "/static/media/black-corded-headset-205926.1e525759.jpg"
+  },
+  {
+    "revision": "9c7e5a454c39059d0c7160129448f99f",
+    "url": "/static/media/candle-3992363_640.9c7e5a45.jpg"
+  },
+  {
+    "revision": "c736e1037de547d3cd1e57e950c86987",
+    "url": "/static/media/caps-1937013_640.c736e103.jpg"
+  },
+  {
+    "revision": "b74997a5dd152dc43750d4c7b4b340cf",
+    "url": "/static/media/cart3.b74997a5.svg"
+  },
+  {
+    "revision": "15b88b6eab29f85cd67f660f44228294",
+    "url": "/static/media/console.15b88b6e.svg"
+  },
+  {
+    "revision": "abcec96285f93243e816f0fbdcf2c16b",
+    "url": "/static/media/engine-505489_640.abcec962.jpg"
+  },
+  {
+    "revision": "d63f537b6e356faa8d18ac0dfa8f71a6",
+    "url": "/static/media/headphones.d63f537b.svg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "3bace13c2d5332b687389f4d09b99838",
+    "url": "/static/media/online-shop.3bace13c.svg"
+  },
+  {
+    "revision": "de6eab7e0934bc722fad52c351820e89",
+    "url": "/static/media/photo-of-a-macbook-air-812264.de6eab7e.jpg"
+  },
+  {
+    "revision": "343e7684bf4320f146df4345a75fd029",
+    "url": "/static/media/responsive.343e7684.svg"
+  },
+  {
+    "revision": "106f474ecf03dc3cf8c678d2fbf521be",
+    "url": "/static/media/rim-2971142_640.106f474e.jpg"
+  },
+  {
+    "revision": "5afc248fc18706ccef776ce1fb117750",
+    "url": "/static/media/semi-opened-laptop-computer-turned-on-on-table-2047905.5afc248f.jpg"
+  },
+  {
+    "revision": "3b9314b2f3853d7ee4f4f378f7328ae4",
+    "url": "/static/media/shopping.3b9314b2.svg"
+  },
+  {
+    "revision": "ef060071e19718dcd2dc39a789b5aaf8",
+    "url": "/static/media/smartwatch.ef060071.svg"
+  },
+  {
+    "revision": "774e2204908319a4cc10ba56808e7441",
+    "url": "/static/media/space-gray-iphone-5s-1294886.774e2204.jpg"
+  },
+  {
+    "revision": "1e8a87156f4b3778e7061a3737ab7a48",
+    "url": "/static/media/spark-plug-32083_640.1e8a8715.png"
+  },
+  {
+    "revision": "e6ea4f96184a592c138277eb725cd9cc",
+    "url": "/static/media/tablet.e6ea4f96.svg"
+  },
+  {
+    "revision": "5d62a91ce1f7103ebc6bcc8f1185c418",
+    "url": "/static/media/transport-3366391_640.5d62a91c.jpg"
+  },
+  {
+    "revision": "b2dda8d9e3571371a41768382bc0369c",
+    "url": "/static/media/tv-monitor.b2dda8d9.svg"
+  },
+  {
+    "revision": "c628a0674466e222168f5e5b6049d73c",
+    "url": "/static/media/web-site.c628a067.png"
+  }
+]);
